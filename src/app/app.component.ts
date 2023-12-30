4d2529ffcd4b8e4dfae76ec3fe06276a43b5bbb8
tebib91 <tebib.ahmed0@gmail.com>
2023-12-30 18:47:42 +0100

    feat(script): add script injector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { SeoService } from './services/seo.service';
+import { ScriptInjectorService } from './services/script-injector.service';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,17 @@ import { SeoService } from './services/seo.service';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-basic';
+  title = 'Ahmed Tabib | Fullstack Developer JavaScript';
   backgroundImageUrl: string;
 
-  constructor(    private seo: SeoService
+  constructor(    private seo: SeoService,
+    private script: ScriptInjectorService,
+
     ) {
     // Set the background image URL dynamically
     this.backgroundImageUrl = `/assets/backgrounds/background-${3}.webp`;
-    this.seo.updateMetaData()
+    this.seo.updateMetaData();
+    this.script.inject('G-HQ721RPGYS');
+
   }
 }
